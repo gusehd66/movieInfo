25c7b0dbeda799b52e084fce031b8d77f4dfa805
gusehd66 <rlagusehd45@naver.com>
2021-04-02 23:11:26 +0900

    카드 클릭시 카드 설명 나오는 기능 추가

--- js/movie.js
+++ js/movie.js
@@ -4,14 +4,30 @@ window.onload = function () {
     var starBg = document.querySelector(".starBg");
     var title = document.querySelector(".title");
     var topBtn = document.querySelector(".topBtn");
+    //var card = document.getElementsByClassName("card");
+    //var cardInfo = document.getElementsByClassName("card_info");
+    var li = document.getElementsByTagName("li");
+
+    for (var i = 0; i < li.length; i++) {
+        li[i].addEventListener("click", function () {
+            if (this.classList[0] == "left") {
+                this.children[1].style.opacity = "1";
+            }
+            else {
+                this.children[0].style.opacity = "1";
+            }
+
+
+        })
+    }
+
 
     //스크롤 이벤트
     window.addEventListener("scroll", function (event) {
         var scroll = this.scrollY;
         starBg.style.transform = "translateY(" + -scroll / 3 + "px)";
-        title.style.transform = "translateY(" + scroll / 1.7 + "px)";
+        title.style.transform = "translateY(" + scroll / 1.8 + "px)";
     });
-
     //텍스트 모션
     for (var i = 0; i < title.querySelectorAll('div').length; i++) {
 
@@ -28,16 +44,21 @@ window.onload = function () {
     //페이지 로드시 스크롤 이동
     TweenMax.to(window, 2, {
         scrollTo: {
-            //y: 500
+            y: 400
         },
         delay: 1.7,
         ease: Power4.easeInOut
     });
 
     //페이지 로드시 포스터 이동
-    TweenMax.from("li", 2, {
-        x: -400,
-        delay: 2.5,
+    TweenMax.from(".left", 2, {
+        x: -1000,
+        delay: 2,
+        ease: Power3.easeInOut
+    });
+    TweenMax.from(".right", 2, {
+        x: 1000,
+        delay: 2,
         ease: Power3.easeInOut
     });
 
@@ -51,6 +72,7 @@ window.onload = function () {
             ease: Power3.easeInOut
         });
     })
+
 
 }
 
